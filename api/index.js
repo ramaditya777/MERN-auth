5b4317890f4c142ea9f5af147eb27850b7a79bde
Ramaditya Panda <ramadityapanda3@gmail.com>
2024-06-28 11:45:07 +0530

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log(`MONGODB connected successfully`);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 app.listen(3000, () => {
   console.log(`Server listining on port 3000`);
 });
