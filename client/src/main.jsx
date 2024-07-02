a597a7c22f3d9ca01902bda6a77097865005d523
Ramaditya Panda <ramadityapanda3@gmail.com>
2024-07-02 12:42:28 +0530

    add redux presist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
-// src/index.js
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom";
+import App from "./App";
 import "./index.css";
-import store from "./redux/store.js"; // Ensure this path is correct
+import store from "./redux/store"; // Check the path to your Redux store
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store"; // Import your persistor from Redux store
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,27 @@
-// src/redux/store.js
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "../redux/user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer, // Pass persistedReducer directly here
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: false, // Disable serializable check for Redux Persist
     }),
 });
 
 export default store;
+export const persistor = persistStore(store);
